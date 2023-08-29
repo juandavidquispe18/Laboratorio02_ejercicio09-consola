be1da85674c65a411dda79b879cf953706af8ff0
USUARIO <USUARIO@DESKTOP-N0DKPVM>
2023-08-28 21:03:15 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ingresar datos
+Console.WriteLine("Ingrese 3 números enteros diferentes: ");
+
+Console.Write("Numero 1: ");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Numero 2: ");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Numero 3: ");
+int numero3 = Convert.ToInt32(Console.ReadLine());
+
+if (numero1 != numero2 && numero1 != numero3 && numero2 != numero3)
+{
+    //Mostrar resultados
+    int mayor = Math.Max(numero1, Math.Max(numero2, numero3));
+    Console.WriteLine($"El mayor de los 3 números es: {mayor}");
+}
+else 
+{
+    Console.WriteLine("Error: Los 3 números ingresados tienen que ser diferentes.");
+}
+Console.ReadKey();
+
+
+
